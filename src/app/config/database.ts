c910c58d40ac1b7ba781bba79d4ef0dbff7a36fc
Pedro Oliveira <Pedroaolive@gmail.com>
2023-07-12 20:48:42 -0300

    config: add configs to connect a database

---
+++ src/app/config/database.ts
@@ -0,0 +1,17 @@
+module.exports = {
+  define: {
+    charset: "utf8mb4",
+    collate: "utf8mb4_bin",
+  },
+  dialect: process.env.DB_DIALECT || "postgres",
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT || 5432,
+  pool: {
+    max: 5,
+    min: 0,
+    idel: 10000,
+  },
+};
