ddeab3d2a9fcd830e921c468718d357e02d77d10
Pedro Oliveira <Pedroaolive@gmail.com>
2023-07-17 02:18:57 -0300

    feat: add taskRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./authRoutes";
+import taskRoute from "./taskRoutes";
 
 const routes = Router();
 
 routes.use("/auth", authRoutes);
+routes.use(taskRoute);
 
 export default routes;
